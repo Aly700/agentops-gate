93da8cb190c2dc06cfda3cb0e9df054aa0459efe
Aly700 <affanyasir9@gmail.com>
2026-08-28 17:03:07 -0400

    spring initializr scaffold: web, jpa, validation, actuator, postgres, flyway, testcontainers, spring cloud aws

---
+++ src/test/java/dev/affan/agentopsgate/AgentopsGateApplicationTests.java
@@ -0,0 +1,15 @@
+package dev.affan.agentopsgate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+@Import(TestcontainersConfiguration.class)
+@SpringBootTest
+class AgentopsGateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
